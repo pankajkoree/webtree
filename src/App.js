af005c2d3270e7743159189ecb4febe0823db2c3
pankajkoree <pankajkori2057@gmail.com>
2024-12-26 21:22:33 +0530

    profile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import "./App.css";
+import { Route, Routes, useNavigate } from "react-router";
+import ProfilePage from "./profile/page";
 
 function App() {
-  return <div className="App"></div>;
+  const navigate = useNavigate();
+
+  const handleProfileClick = () => {
+    navigate("/profile");
+  };
+
+  return (
+    <div className="App relative flex justify-center mt-12">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <button
+              className="relative flex px-4 py-2 text-xl border-2 border-green-500 hover:bg-green-500 hover:text-white"
+              onClick={handleProfileClick}
+            >
+              Profile
+            </button>
+          }
+        />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all files in the 'src' directory
+    "./components/**/*.{js,jsx,ts,tsx}", // Include all files in the 'components' directory
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Customizations for your theme (colors, fonts, etc.) can go here
+    },
   },
   plugins: [],
-}
+};
